8f3a8a66c6719392c9fe4b941b956bc01f18376c
winifredogbeiwi <imadeogbeiwi@gmail.com>
2024-03-04 20:12:10 +0100

    FIX:restructure of componenets

--- src/pages/web/Home.jsx
+++ src/pages/web/Home.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
-import Button from '../../components/common/Button'
-import Images from '../../constant/assest'
+import Hero from '../../components/home/hero.component'
 
 const Home = () => {
   return (
     <>
-      <main className='hero-container flex'>
-        <div className='hero-content-container flex'>
-          <h1>
-            File storage and sharing for remote teams that work together on <span>Web3</span>
-          </h1>
-          <p>Deupload is a online file manager on decentralized cloud storage and IPFS that allows you storage, share, collect files privately and team collaboration without subscription.</p>
-          <div className="nav-buttons">
-            <Button text="Get Started" specific="nav-button" location="" />
-            <Button text="Learn More" specific="nav-button2" location="" />
-          </div>
-        </div>
-
-        <img src={Images.HeroImg} alt=""/>
-      </main>
+   <Hero/>
     </>
   )
 }
