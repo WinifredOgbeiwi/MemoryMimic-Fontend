bb068c3d6251d7106fbc0e47c00cf6f251f28b13
winifredogbeiwi <imadeogbeiwi@gmail.com>
2024-03-05 09:29:25 +0100

    CREATION:feature section

--- src/components/home/features.component.jsx
+++ src/components/home/features.component.jsx
@@ -5,31 +5,41 @@ import { Heading, SubHeadingParagraphs, SubSubHeadings, } from '../common/Texts'
 
 const Features = () => {
     return (
-        <section>
+        <section className='feature-container '>
             <div className='title'>
                 <Heading text="Lightweight design, ready to use for " span="team productivity." />
             </div>
 
             <div className='feature-boxes'>
-                <div className='box'>
+                <div className='box box1'>
                     <SubSubHeadings text="File Sharing" color="#651FFF" />
                     <SubHeadingParagraphs text="Securely share files within or outside your organization, and control access, track edits, and analyze the shared content stats." />
                     <img src={Images.FeatureImage1} alt="" />
                 </div>
-                <div className='box'>
+                <div className='box box2'>
                     <SubSubHeadings text="File Sharing" color="#651FFF" />
-                    <SubHeadingParagraphs text="Securely share files within or outside your organization, and control access, track edits, and analyze the shared content stats."/>
-                
+                    <SubHeadingParagraphs text="Securely share files within or outside your organization, and control access, track edits, and analyze the shared content stats." />
+
                     <img src={Images.FeatureImage2} alt="" />
                 </div>
                 <div className='box'>
+
                     <div>
-                        <h2>Team collaboration in  one simple place with privacy come first.</h2>
-                        <p>Securely share and work together with Deupload’s simple file storage and sharing, easy user management, unlimited file size, password protected links and more.</p>
-                        <Button text="Start Now" />
-                        <Button text="More Feature" />
+                        <h3><span>Team collaboration</span> in  one simple place with privacy come first.</h3>
+                        <SubHeadingParagraphs text="Securely share and work together with Deupload’s simple file storage and sharing, easy user management, unlimited file size, password protected links and more." />
+                        <div className='nav-buttons'>
+                                <Button text="Start Now" specific="nav-button" />
+                        <Button text="More Feature" specific="nav-button2" />
+                        </div>
+                    
                     </div>
-                    <img src={Images.FeatureImage3} alt="" />
+
+            <div>
+                  <img src={Images.FeatureImage3} alt="" />
+            </div>
+                       
+              
+                   
 
                 </div>
             </div>
